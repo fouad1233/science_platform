236ec377a29136c7603339b4818c519771a7ea73
Fouad Aladhami <fouadiadhami@gmail.com>
2023-02-11 12:55:07 +0300

    Temperature readed sucessfully but there is an error in pressure

--- esp32u_bmp280_pressure_sensor/esp32u_bmp280_pressure_sensor.ino
+++ esp32u_bmp280_pressure_sensor/esp32u_bmp280_pressure_sensor.ino
@@ -1,57 +1,70 @@
-/***************************************************************************
-This is a library for the BMP280 humidity, temperature & pressure sensor
- 
-Designed specifically to work with the Adafruit BMEP280 Breakout
-----> http://www.adafruit.com/products/2651
- 
-These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-to interface.
- 
-Adafruit invests time and resources providing this open source code,
-please support Adafruit andopen-source hardware by purchasing products
-from Adafruit!
- 
-Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-BSD license, all text above must be included in any redistribution
-***************************************************************************/
- 
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
- 
-#define BMP_SCK 13
-#define BMP_MISO 12
-#define BMP_MOSI 11
-#define BMP_CS 10
- 
-Adafruit_BMP280 bme; // I2C
-//Adafruit_BMP280 bme(BMP_CS); // hardware SPI
-//Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO, BMP_SCK);
- 
-void setup() {
-  Serial.begin(115200);
-  Serial.println(F("BMP280 test"));
-   
-  if (!bme.begin()) {
-  Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-  while (1);
+/*!
+ * @file config.ino
+ * @brief Download this demo to test config to bmp280, connect sensor through IIC interface.
+ * @n  Data will print on your serial monitor.
+ *
+ * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @license     The MIT License (MIT)
+ * @author [Frank](jiehan.guo@dfrobot.com)
+ * @version  V1.0
+ * @date  2019-03-12
+ * @url https://github.com/DFRobot/DFRobot_BMP280
+ */
+
+#include "DFRobot_BMP280.h"
+#include "Wire.h"
+
+typedef DFRobot_BMP280_IIC    BMP;    // ******** use abbreviations instead of full names ********
+
+BMP   bmp(&Wire, BMP::eSdoLow);
+
+#define SEA_LEVEL_PRESSURE    1015.0f   // sea level pressure
+
+// show last sensor operate status
+void printLastOperateStatus(BMP::eStatus_t eStatus)
+{
+  switch(eStatus) {
+  case BMP::eStatusOK:    Serial.println("everything ok"); break;
+  case BMP::eStatusErr:   Serial.println("unknow error"); break;
+  case BMP::eStatusErrDeviceNotDetected:    Serial.println("device not detected"); break;
+  case BMP::eStatusErrParameter:    Serial.println("parameter error"); break;
+  default: Serial.println("unknow status"); break;
   }
 }
- 
-void loop() {
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" *C");
-   
-  Serial.print("Pressure = ");
-  Serial.print(bme.readPressure());
-  Serial.println(" Pa");
-   
-  Serial.print("Approx altitude = ");
-  Serial.print(bme.readAltitude(1013.25)); // this should be adjusted to your local forcase
-  Serial.println(" m");
-   
+
+void setup()
+{
+  Serial.begin(115200);
+  bmp.reset();
+  Serial.println("bmp config test");
+  while(bmp.begin() != BMP::eStatusOK) {
+    Serial.println("bmp begin faild");
+    printLastOperateStatus(bmp.lastOperateStatus);
+    delay(2000);
+  }
+  Serial.println("bmp begin success");
+
+  bmp.setConfigFilter(BMP::eConfigFilter_off);        // set config filter
+  bmp.setConfigTStandby(BMP::eConfigTStandby_125);    // set standby time
+  bmp.setCtrlMeasSamplingTemp(BMP::eSampling_X8);     // set temperature over sampling
+  bmp.setCtrlMeasSamplingPress(BMP::eSampling_X8);    // set pressure over sampling
+  bmp.setCtrlMeasMode(BMP::eCtrlMeasModeNormal);     // set control measurement mode to make these settings effective
+
+  delay(100);
+}
+
+void loop()
+{
+  float   temp = bmp.getTemperature();
+  uint32_t    press = bmp.getPressure();
+  float   alti = bmp.calAltitude(SEA_LEVEL_PRESSURE, press);
+
   Serial.println();
-  delay(2000);
+  Serial.println("======== start print ========");
+  Serial.print("temperature (unit Celsius): "); Serial.println(temp);
+  Serial.print("pressure (unit pa):         "); Serial.println(press);
+  Serial.print("altitude (unit meter):      "); Serial.println(alti);
+  Serial.println("========  end print  ========");
+
+  delay(1000);
 }
