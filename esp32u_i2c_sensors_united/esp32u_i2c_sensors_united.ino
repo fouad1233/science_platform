5c4aca8039a1014fa10c957ddf2f9055bef098f9
Erhan Gök <erhan_gk08@outlook.com>
2023-02-18 10:53:32 +0300

    Update esp32u_i2c_sensors_united.ino

--- esp32u_i2c_sensors_united/esp32u_i2c_sensors_united.ino
+++ esp32u_i2c_sensors_united/esp32u_i2c_sensors_united.ino
@@ -28,6 +28,26 @@ float alti;
 typedef DFRobot_BMP280_IIC    BMP;    // ******** use abbreviations instead of full names ********
 BMP   bmp(&Wire, BMP::eSdoLow);
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
+SI114X SI1145 = SI114X(); // initialise sunlight sensor
+
+
+void setup()
+{
+  Serial.begin(115200);
+  SI1145_setup();
+  BMP280_setup();
+  TSL2591_setup();
+  delay(1000);
+//////////
+}
+
+void loop()
+{
+  readSensor();
+  delay(1000);
+}
+
+
 
 // show BMP280 last sensor operate status
 void printLastOperateStatus(BMP::eStatus_t eStatus)
@@ -51,50 +71,25 @@ void printLastOperateStatus(BMP::eStatus_t eStatus)
   }
 }
 
-SI114X SI1145 = SI114X(); // initialise sunlight sensor
 
-void sendSensor() // function to read sensor values and print with Serial library
+void readSensor() // function to read sensor values and print with Serial library
 {
-//////// UV Sensor
-  visible = SI1145.ReadVisible(); // visible radiation
-  ir = SI1145.ReadIR(); // IR radiation
-  uv = SI1145.ReadUV(); // UV index
+
+  read_SI1145(); // UV Sensor
 
 //////// Pressure Sensor
   
   temp = bmp.getTemperature();
   press = bmp.getPressure();
   alti = bmp.calAltitude(SEA_LEVEL_PRESSURE, press);
-//////// tsl2591
-  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
-  // That way you can do whatever math and comparisons you want!
-  uint32_t lum = tsl.getFullLuminosity();
-  uint16_t ir, full;
-  ir = lum >> 16;
-  full = lum & 0xFFFF;
 
-  Serial.print("\n======== UV SENSOR ========\n");
-  Serial.print("visible radiation: ");
-  Serial.println(visible);
-  Serial.print("IR radiation: ");
-  Serial.println(ir);
-  Serial.print("UV index: ");
-  Serial.println(uv/100);
-  
   Serial.print("\n======== BMP280 ========\n");
   Serial.print("temperature (unit Celsius): "); Serial.println(temp);
   Serial.print("pressure (unit pa):         "); Serial.println(press);
   Serial.print("altitude (unit meter):      "); Serial.println(alti);
   Serial.println("========  end print  ========");
 
-  Serial.print("\n======== TSL2591 ========\n");
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
-  Serial.print(F("\nFull: ")); Serial.print(full); Serial.print(F("  "));
-  Serial.print(F("\nVisible: ")); Serial.print(full - ir); Serial.print(F("  "));
-  Serial.print(F("\nLux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
-  Serial.println("========  end print  ========");
-  
+  advancedRead_TSL2591(); // light sensor
 }
 
 
@@ -127,6 +122,8 @@ void BMP280_setup()
   bmp.setCtrlMeasSamplingPress(BMP::eSampling_X8);    // set pressure over sampling
   bmp.setCtrlMeasMode(BMP::eCtrlMeasModeNormal);     // set control measurement mode to make these settings effective
 }
+
+
 void TSL2591_setup()
 { 
   
@@ -170,7 +167,22 @@ void TSL2591_setup()
     Serial.println(F(""));
   }
   
-  
+void read_SI1145(void)
+{
+  visible = SI1145.ReadVisible(); // visible radiation
+  ir = SI1145.ReadIR(); // IR radiation
+  uv = SI1145.ReadUV(); // UV index
+
+  Serial.print("\n======== UV SENSOR ========\n");
+  Serial.print("visible radiation: ");
+  Serial.println(visible);
+  Serial.print("IR radiation: ");
+  Serial.println(ir);
+  Serial.print("UV index: ");
+  Serial.println(uv/100);
+  Serial.println("========  end print  ========");
+
+}
 
 void advancedRead_TSL2591(void)
 {
@@ -180,50 +192,12 @@ void advancedRead_TSL2591(void)
   uint16_t ir, full;
   ir = lum >> 16;
   full = lum & 0xFFFF;
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
-  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
-  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
+  Serial.print("\n======== TSL2591 ========\n");
+  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] \n"));
+  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  \n"));
+  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  \n"));
+  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  \n"));
   Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
-}
-void setup()
-{
-  // Serial.begin(115200);
-  // Serial.println("Beginning Si1145!");
-  // while (!SI1145.Begin())
-  // {
-  //   Serial.println("Si1145 is not ready!");
-  //   delay(1000);
-  // }
-  // Serial.println("Si1145 is ready!");
-  //delay(1000);
-
-/////////
-  // bmp.reset();
-  // Serial.println("bmp config test");
-  // while(bmp.begin() != BMP::eStatusOK) {
-  //   Serial.println("bmp begin faild");
-  //   printLastOperateStatus(bmp.lastOperateStatus);
-  //   delay(2000);
-  // }
-  // Serial.println("bmp begin success");
-
-  // bmp.setConfigFilter(BMP::eConfigFilter_off);        // set config filter
-  // bmp.setConfigTStandby(BMP::eConfigTStandby_125);    // set standby time
-  // bmp.setCtrlMeasSamplingTemp(BMP::eSampling_X8);     // set temperature over sampling
-  // bmp.setCtrlMeasSamplingPress(BMP::eSampling_X8);    // set pressure over sampling
-  // bmp.setCtrlMeasMode(BMP::eCtrlMeasModeNormal);     // set control measurement mode to make these settings effective
-  //delay(100);
-  Serial.begin(115200);
-  SI1145_setup();
-  BMP280_setup();
-  TSL2591_setup();
-  delay(1000);
-//////////
+  Serial.println("========  end print  ========");
 }
 
-void loop()
-{
-  sendSensor();
-  delay(1000);
-}
